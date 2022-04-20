419c15734f922e87d023dbe8a262e01f9fe1a95e
viettask <viet.nguyen@viettask.com>
2022-04-20 20:51:21 +1000

    airBNB

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import App from './components/App';
+
+// C:\Users\vietn\OneDrive\Desktop\Full Stack\airBnb\components\App.js
+// C:\Users\vietn\OneDrive\Desktop\Full Stack\airBnb\index.js
+// C:\Users\vietn\OneDrive\Desktop\Full Stack\airBnb\public\index.html
+
+ReactDOM.render(
+ <App />,
+  document.getElementById('root')
+);
+
+
+
